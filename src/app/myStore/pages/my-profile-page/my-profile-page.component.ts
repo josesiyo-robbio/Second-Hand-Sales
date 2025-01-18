5db983af651c45b3d24836af5f6b8d27995f13be
josesiyo-robbio <jose.robles.bo@gmail.com>
2025-01-17 21:51:42 -0600

    profile finish

--- src/app/myStore/pages/my-profile-page/my-profile-page.component.ts
+++ src/app/myStore/pages/my-profile-page/my-profile-page.component.ts
@@ -1,10 +1,4 @@
 import {Component, model} from '@angular/core';
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
 
 @Component({
   selector: 'app-my-profile-page',
