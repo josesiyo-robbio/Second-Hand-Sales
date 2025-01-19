d44a188aef43555ec471a243673648ecba70d95f
josesiyo-robbio <jose.robles.bo@gmail.com>
2025-01-18 20:21:22 -0600

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatButton} from '@angular/material/button';
-import {MatSidenavContainer, MatSidenavModule} from '@angular/material/sidenav';
-import {MatListItem, MatNavList} from '@angular/material/list';
-import {MatToolbar} from '@angular/material/toolbar';
-import {SharedModule} from './shared/shared.module';
 import 'animate.css';
+
+//MODULES
+import { AppRoutingModule } from './app-routing.module';
+import {SharedModule} from './shared/shared.module';
 
 
 
@@ -18,12 +16,6 @@ import 'animate.css';
   [
     AppRoutingModule,
     BrowserModule,
-    MatButton,
-    MatListItem,
-    MatNavList,
-    MatSidenavContainer,
-    MatSidenavModule,
-    MatToolbar,
     SharedModule,
   ],
   providers: [provideAnimationsAsync()],
--- src/app/myStore/my-store.module.ts
+++ src/app/myStore/my-store.module.ts
@@ -1,48 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 
+//MY COMPONENTS & MODULES
 import { MyStoreRoutingModule } from './my-store-routing.module';
+import { StatusProductPageComponent } from './pages/status-product-page/status-product-page.component';
 import {SidebarComponent} from './components/sidebar/sidebar.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import { SellProductComponent } from './pages/sell-product/sell-product.component';
+import { NewProductComponent } from './pages/new-product-page/new-product.component';
+import { SellHistoryPageComponent } from './pages/sell-history-page/sell-history-page.component';
+import { MyProfilePageComponent } from './pages/my-profile-page/my-profile-page.component';
+
+//MATERIAL YOU
 import {MatSidenavContainer, MatSidenavModule} from '@angular/material/sidenav';
 import {MatListItem, MatNavList} from '@angular/material/list';
-import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
+import {MatProgressBar} from '@angular/material/progress-bar';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {MatButton, MatFabAnchor, MatFabButton, MatIconButton, MatMiniFabButton} from '@angular/material/button';
 import {MatToolbar} from '@angular/material/toolbar';
-import { SellProductComponent } from './pages/sell-product/sell-product.component';
-import {
-  MatCardActions,
-  MatCardContent,
-  MatCardHeader,
-  MatCardImage,
-  MatCardModule
-} from '@angular/material/card';
-import { NewProductComponent } from './pages/new-product-page/new-product.component';
+import {MatCardActions, MatCardContent, MatCardHeader, MatCardImage, MatCardModule } from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatOption, MatSelect} from '@angular/material/select';
 import {MatInput} from '@angular/material/input';
-import { SellHistoryPageComponent } from './pages/sell-history-page/sell-history-page.component';
-import {
-  MatCell, MatCellDef,
-  MatColumnDef,
-  MatHeaderCell,
-  MatHeaderCellDef,
-  MatHeaderRow, MatHeaderRowDef, MatNoDataRow,
-  MatRow,
-  MatTable, MatTableModule
-} from '@angular/material/table';
+import {MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatNoDataRow, MatRow, MatTable, MatTableModule } from '@angular/material/table';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatSort, MatSortModule} from '@angular/material/sort';
-import { MyProfilePageComponent } from './pages/my-profile-page/my-profile-page.component';
 import {MatGridList, MatGridTile} from '@angular/material/grid-list';
 import {MatCalendar, MatDatepickerModule} from '@angular/material/datepicker';
 import {DateAdapter, MAT_DATE_FORMATS} from '@angular/material/core';
 import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
 import {MatIcon} from '@angular/material/icon';
-import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
-import {ReactiveFormsModule} from '@angular/forms';
-import { StatusProductPageComponent } from './pages/status-product-page/status-product-page.component';
-import {MatProgressBar} from '@angular/material/progress-bar';
+
+
 
 @NgModule({
   declarations:
@@ -56,7 +47,8 @@ import {MatProgressBar} from '@angular/material/progress-bar';
     MyProfilePageComponent,
     StatusProductPageComponent
   ],
-  imports: [
+  imports:
+  [
     MatSidenavModule,
     CommonModule,
     MyStoreRoutingModule,
@@ -103,16 +95,14 @@ import {MatProgressBar} from '@angular/material/progress-bar';
     MatFabButton,
     ReactiveFormsModule,
     MatProgressBar,
-
-
   ],
   exports: [],
   providers: [
-    {
-      provide: DateAdapter,
-      useClass: MomentDateAdapter,
-    },
-    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+  {
+    provide: DateAdapter,
+    useClass: MomentDateAdapter,
+  },
+  { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
   ],
 })
 export class MyStoreModule { }
